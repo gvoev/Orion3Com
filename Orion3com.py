f43251ec5976c256fd38131df28e0137a7e8b07c
gvoev <58187814+gvoev@users.noreply.github.com>
2020-05-04 20:02:30 +0300

    pargs

--- Orion3com.py
+++ Orion3com.py
@@ -15,28 +15,19 @@ ser = serial.Serial(
 
 delay = 1
 
+hello = 1
 
-
-
-
-def go(a):
+def go(*pargs):
     print(ser.readall().decode('utf-8'))
-    ser.write(a)
+    for arg in pargs:
+        ser.write(arg)
     time.sleep(delay)
 
 def setBR(PAM,baserate,channel):
     go(b'\n')
     go(b'3\n')
-    go(b'pam ')
-    go(bytes(str(PAM), encoding = 'utf-8'))
-    go(b' ')
-    go(bytes(str(channel), encoding = 'utf-8'))
-    go(b'\n')
-    go(b'baserate ')
-    go(bytes(str(baserate), encoding = 'utf-8'))
-    go(b' ')
-    go(bytes(str(channel), encoding = 'utf-8'))
-    go(b'\n')
+    go(b'pam ', bytes(str(PAM), encoding = 'utf-8'), b' ', bytes(str(channel), encoding = 'utf-8'), b'\n')
+    go(b'baserate ', bytes(str(baserate), encoding = 'utf-8'), b' ', bytes(str(channel), encoding = 'utf-8'), b'\n')
     go(b'm\n')
     
 print(ser.read_until('Exit',200).decode('utf-8'))
@@ -45,86 +36,3 @@ print(ser.readall().decode('utf-8'))
 #setBR(32,88,1)
 
 ser.close()
-
-
-
-
-
-musor = '''
-go(b'm\n')
-go(b'3\n')
-go(b'1\n')
-go(b'1\n')
-go(b'1\n')
-go(b'off')
-go(b'\n')
-go(b'2\n')
-go(b'239.239.123.123\n')
-go(b'3\n')
-go(b'1234\n')
-go(b'm\n')
-go(b'm\n')
-go(b'm\n')
-go(b'm\n')
-time.sleep(1)
-text = ser.readall().decode('utf-8')
-print(text)
-ser.close()
-
-
-
-ser.write(b'm\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.cancel_read()
-ser.write(b'm\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'3\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'1\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'1\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'1\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'off')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.write(b'm\n')
-print(ser.readall().decode('utf-8'))
-ser.write(b'm\n')
-time.sleep(delay)
-print(ser.readall().decode('utf-8'))
-ser.cancel_read()
-ser.write(b'm\n')
-time.sleep(delay)
-ser.close()
-
-
-
-z = ser.readlines(100@q)
-ser.write(b'\n')
-ser.seek(b'Fault')
-z = ser.readlines(10)
-time.sleep(0.2)
-ser.write(b'3')
-a = ser.readlines(10)
-time.sleep(0.2)
-ser.write(b'1')
-time.sleep(0.2)
-b = ser.readlines(10)
-time.sleep(0.2)
-ser.close()
-print(a, '//n', b , '//n', z)
-'''
